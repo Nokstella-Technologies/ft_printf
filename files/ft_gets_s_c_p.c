217313b19ecf6ac1414f2c5d0f5a9dcf3a10fd0e
luizlcezario <luizlcezario@gmail.com>
2021-10-08 17:27:17 -0300

    final

--- files/ft_gets_s_c_p.c
+++ files/ft_gets_s_c_p.c
@@ -6,7 +6,7 @@
 /*   By: llima-ce <llima-ce@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 19:01:01 by llima-ce          #+#    #+#             */
-/*   Updated: 2021/10/08 17:20:30 by llima-ce         ###   ########.fr       */
+/*   Updated: 2021/10/08 17:27:13 by llima-ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,8 @@ void	ft_get_string(t_format *buffer)
 	int		len;
 
 	buffer->formated_src = ft_strdup(va_arg(buffer->args_c, char *));
-	if (buffer->formated_src)
-	{
-		len = (int)ft_strlen(buffer->formated_src);
-		pointer_flag(buffer, &len);
-	}
-	else
-	{
-		free(buffer->formated_src);
-		buffer->formated_src = ft_strdup("(null)");
-		len = (int)ft_strlen(buffer->formated_src);
-	}
+	len = (int)ft_strlen(buffer->formated_src);
+	pointer_flag(buffer, &len);
 	min_width_flag(buffer, &len);
 	buffer->len += len;
 }
