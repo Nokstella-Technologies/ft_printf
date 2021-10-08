6a0276c8116273703527eb4835043eb6fb33a5bf
luizlcezario <luizlcezario@gmail.com>
2021-10-08 18:14:28 -0300

    final

--- files/ft_check_errors.c
+++ files/ft_check_errors.c
@@ -6,7 +6,7 @@
 /*   By: llima-ce <llima-ce@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 18:19:50 by llima-ce          #+#    #+#             */
-/*   Updated: 2021/10/08 18:11:46 by llima-ce         ###   ########.fr       */
+/*   Updated: 2021/10/08 18:14:25 by llima-ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	ft_new_errors(t_format *buffer,int type)
 {
-	if(type == 1)
+	if (type == 1)
 	{
 		buffer->len = buffer->len + 1;
-		write(1,"%", 1);
-		return(1);
+		write(1, "%", 1);
+		return (1);
 	}
-	else if(type == 2)
+	else if (type == 2)
 	{
-		return(2);
+		write(1, " ", 1);
+		return (2);
 	}
-	return(1);
+	return (1);
 }
